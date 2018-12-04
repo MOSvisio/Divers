780daeb2ac336ead7f61fccd37909c52327d10e1
MOSvisio <lucas.schutz0954@gmail.com>
2018-12-04 11:39:09 +0100

    Add files via upload
    now you can see the result directly on the website

--- sketch.js
+++ sketch.js
@@ -26,6 +26,12 @@ function draw() {
     }
     total++;
   pie = 4*(reussi/float(total));
-
+  fill(255);
+  noStroke();
+  rect(300,0,350,200);
+  fill(0);
+  textSize(32);
+  text(pie, 300, 50);
+  text(total,300,100);
   print(pie);
 }
