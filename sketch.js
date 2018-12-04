4e850f43521f1b661e900da0bbd841a328bc317d
MOSvisio <lucas.schutz0954@gmail.com>
2018-12-04 10:39:44 +0100

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,31 @@
+var x;
+var y;
+var reussi = 0;
+var total = 0;
+var pie;
+var r = 250
+
+
+function setup() {
+  createCanvas(1000, 500);
+  background(255);
+  ellipse(0,0, r*2,r*2);
+}
+
+function draw() {
+    x = int(random(0,101))*5;
+    y = int(random(0,101))*5;
+    if(Math.sqrt(Math.pow(x,2) + Math.pow(y,2)) <= r*2){
+      fill(210);
+      ellipse(x/2,y/2,5,5);
+      reussi++;
+
+    } else {
+      fill(0);
+      ellipse(x/2,y/2,5,5);
+    }
+    total++;
+  pie = 4*(reussi/float(total));
+
+  print(pie);
+}
